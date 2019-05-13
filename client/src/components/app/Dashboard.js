fd08fc3240ec16ee8dbae2926024e3d491840edf
Jordan Olsen <jolsen@neslotech.co.za>
2019-05-13 20:35:39 +0000

    POS-22: created the dashboard screen.

--- client/src/components/app/Dashboard.js
+++ client/src/components/app/Dashboard.js
@@ -4,10 +4,44 @@ import './Dashboard.scss';
 
 export default class Dashboard extends React.Component {
 
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            user: {}
+        };
+    }
+
     render() {
         return (
-            <main>
-                This is the dashboard!
+            <main className="container">
+                <header className="text-center">
+                    <h1>Welcome, {this.state.user.name}</h1>
+                    <h4>@{this.state.user.username}</h4>
+                    <h5 className="text-uppercase">{this.state.user.group}</h5>
+                </header>
+                <main className="d-flex">
+                    <section className="col-6 card">
+                        <header className="card-img-top">
+                            <span><i className="fa fa-address-book"/></span>
+                        </header>
+                        <main className="card-body">
+                            <h5 className="card-title">Stock Management</h5>
+                            <p className="card-text">Manage the stock that is available within the shop!</p>
+                            <button className="btn btn-primary">Go now!</button>
+                        </main>
+                    </section>
+                    <section className="col-6 card">
+                        <header className="card-img-top">
+                            <span><i className="fa fa-address-book"/></span>
+                        </header>
+                        <main className="card-body">
+                            <h5 className="card-title">Record Transactions</h5>
+                            <p className="card-text">Record transactions for a customer!</p>
+                            <button className="btn btn-primary">Go now!</button>
+                        </main>
+                    </section>
+                </main>
             </main>
         )
     }
--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import 'bootstrap/dist/css/bootstrap.css';
+import 'font-awesome/css/font-awesome.min.css';
 import 'toastr/build/toastr.min.css'
 
 import './index.scss';
