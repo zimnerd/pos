6ab19c209590fb6ce2474d1b1582bd40ce834217
Jordan Olsen <jolsen@neslotech.co.za>
2019-05-19 16:18:58 +0000

    POS-57: created the Combo model.

---
+++ database/migrations/2019_05_19_141613_create_combos_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCombosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::dropIfExists('combostyle');
+        Schema::create('combostyle', function (Blueprint $table) {
+            $table->string('code')->primary();
+            $table->string('description');
+            $table->date('startdate');
+            $table->date('enddate');
+            $table->tinyInteger('active');
+            $table->dateTime('dts');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('combostyle');
+    }
+}
